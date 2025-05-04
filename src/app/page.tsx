4dbe83a9345630d540eebe543f7b31bb43374400
danielqueirozc <danielqueiroz.job@gmail.com>
2025-05-04 15:28:52 -0300

    feat: add header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
+import { Header } from "@/components/header.";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
-  <div>
-    <h1 className="text-3xl bg-amber-300 m-4 text-amber-50">Bem vindo</h1>
-    <h2 className="text-3xl bg-amber-300 m-4 text-amber-50 p-4"> Bem vindo</h2>
-  </div>
+    <div>
+      <Header />
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Ola, Daniel!</h2>
+        <p>Sabado, 3 de Maio.</p>
+
+        <div className="flex items-center gap-2 mt-6">
+          <Input placeholder="Faca sua busca..." />
+          <Button size="icon" variant="outline">
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative w-full h-[150px] mt-6">
+          <Image
+            src="/banner-01.png"
+            fill
+            className="object-cover rounded-b-xl"
+            alt="banner"
+          />
+        </div>
+      </div>
+    </div>
   );
 }
