dd5a3073735de2ed77a228eb9cd2bf16e2a64862
danielqueirozc <danielqueiroz.job@gmail.com>
2025-05-10 16:27:54 -0300

    feat: add barbershopitem component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import Image from "next/image";
 import { Header } from "@/components/header.";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
-import Image from "next/image";
+import { db } from "@/lib/prisma";
+import { BarberShopItem } from "@/components/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+  const barbershops = await db.barbershop.findMany()
+
+  console.log(barbershops)
+
   return (
     <div>
       <Header />
@@ -28,7 +37,49 @@ export default function Home() {
             alt="banner"
           />
         </div>
+
+
+        <h2 className="uppercase font-bold mt-6 mb-3 text-gray-400 text-xs">
+          agendamentos
+        </h2>
+        <Card className="mt-6 p-0">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge>
+                Confirmado
+              </Badge>
+
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="w-6 h-6">
+                  <AvatarImage src="https://utfs.io/f/5832df58-cfd7-4b3f-b102-42b7e150ced2-16r.png" />
+                </Avatar>
+
+                <p className="text-sm">Barbearia do Daniel</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 px-5">
+              <p className="text-sm">Agosto</p>
+              <span className="text-2xl">04</span>
+              <span className="text-sm">20:00</span>
+            </div>
+          </CardContent>
+        </Card>
+
+        <h2 className="uppercase font-bold mt-6 mb-3 text-gray-400 text-xs">
+          recomendados
+        </h2>
+
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          { barbershops.map(barbershop => {
+              return <BarberShopItem key={barbershop.id} barbershop={barbershop} /> 
+            }) 
+          }
+        </div>
       </div>
     </div>
   );
 }
+
